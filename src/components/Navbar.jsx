30bafd0160ed8c94b408f3f4a4d784deff8200bc
SOFTAPPP <aritradatt39@gmail.com>
2026-08-20 22:02:10 +0530

    First-commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="nav-brand">Portfolio.</div>
+      <div className="nav-brand">
+        <img src="/aritra.jpeg" alt="Aritra Dutta" className="nav-profile-img" />
+        Portfolio.
+      </div>
       <ul className="nav-links">
         <li><a href="#about">About</a></li>
         <li><a href="#education">Education</a></li>
